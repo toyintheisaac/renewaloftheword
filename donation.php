2846f70866eeac51af1e8e8bc3cd735aeef53459
Isaac Toyin <58235609+iamisaacp@users.noreply.github.com>
2021-03-11 13:35:12 -0800

    isaac

--- donation.php
+++ donation.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html> 
 <html lang="en" >
 <head>
-	<title>Donate</title> 
+	<title>Donates</title> 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
